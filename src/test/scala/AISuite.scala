8d9715cf2aa4ad010eed28160c017307ca5f7476
Samuel Heaney <sheaney@gmail.com>
2013-04-06 17:48:06 -0600

    Add another unit test

--- src/test/scala/AISuite.scala
+++ src/test/scala/AISuite.scala
@@ -19,7 +19,8 @@ class AISuite extends FunSuite with BeforeAndAfter {
     assert(AI.search(board, new Computer) === (0, 0))
   }
 
-  // Starting from a corner, the Computer should always choose the center to not lose eventually
+  // If a corner was taken on the first move, the CPU should choose the center 
+  // as the second move to not loose eventually
   test("search: should return move (1, 1)") {
     repr(0)(0) = 1
     val board = new Board(repr)
@@ -32,5 +33,16 @@ class AISuite extends FunSuite with BeforeAndAfter {
     val move = AI.search(board, new Computer)
     val check = move == (0, 0) || move == (0, 2) || move == (2, 0) || move == (2, 2)
     assert(check, s"CPU did not choose corner move, it chose ${move}")
+  }
+
+  // CPU should win when possible
+  test("search: should pick winning move") {
+    repr(0)(0) = 1
+    repr(0)(1) = 1
+    repr(1)(0) = 1
+    repr(2)(0) = 2
+    repr(1)(1) = 1
+    val board = new Board(repr)
+    assert(AI.search(board, new Computer) === (0, 2))
   }
 }
