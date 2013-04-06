6c2d707211cec45532d3a4d578c04b26ddc8aef0
Samuel Heaney <sheaney@gmail.com>
2013-04-06 13:32:24 -0600

    Add more tests

--- src/test/scala/AISuite.scala
+++ src/test/scala/AISuite.scala
@@ -1,17 +1,37 @@
 import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
 
-import org.scalatest.junit.JUnitRunner
-
-class AISuite extends FunSuite {
+class AISuite extends FunSuite with BeforeAndAfter {
   import Game._
 
+  var repr: Array[Array[Int]] = _
+
+  before {
+    repr = Array.ofDim[Int](3,3)
+  }
+
+  // Computer should block winning move
   test("search: should return move (0, 0)") {
-    val repr = Array.ofDim[Int](3,3)
     repr(0)(1) = 1
     repr(0)(2) = 1
     repr(1)(1) = 2
     repr(1)(2) = 2
     val board = new Board(repr)
     assert(AI.search(board, new Computer) === (0, 0))
+  }
+
+  // Starting from a corner, the Computer should always choose the center to not lose eventually
+  test("search: should return move (1, 1)") {
+    repr(0)(0) = 1
+    val board = new Board(repr)
+    assert(AI.search(board, new Computer) === (1, 1))
+  }
+
+  // When CPU starts, it should always choose a corner which maximizes its chance to win
+  test("search: should return a corner move") {
+    val board = new Board(repr)
+    val move = AI.search(board, new Computer)
+    val check = move == (0, 0) || move == (0, 2) || move == (2, 0) || move == (2, 2)
+    assert(check, s"CPU did not choose corner move, it chose ${move}")
   }
 }
